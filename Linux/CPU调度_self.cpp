fd90824a023fbad479169f7f1eadfeb8c8762824
wxt2016213034 <756906795@qq.com>
2022-02-14 18:50:40 +0000

    feat: test

--- Linux/CPU调度_self.cpp
+++ Linux/CPU调度_self.cpp
@@ -5,30 +5,33 @@
 #include <thread>
 #include <pthread.h>
  
-std::mutex iomutex;
+// std::mutex iomutex;
 void f(int num)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
  
     sched_param sch;
     int policy; 
+    while(1){
     pthread_getschedparam(pthread_self(), &policy, &sch);
-    std::lock_guard<std::mutex> lk(iomutex);
+    // std::lock_guard<std::mutex> lk(iomutex);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "Thread " << num << " is executing at priority "
               << sch.sched_priority << '\n';
+    }
 }
  
 int main()
 {
     std::thread t1(f, 1), t2(f, 2);
- 
+    
     sched_param sch;
     int policy; 
     pthread_getschedparam(t1.native_handle(), &policy, &sch);
     sch.sched_priority = 20;
+    std::cout<<"set"<<std::endl;
     if (pthread_setschedparam(t1.native_handle(), SCHED_FIFO, &sch)) {
         std::cout << "Failed to setschedparam: " << std::strerror(errno) << '\n';
     }
- 
+    std::cout<<"set down"<<std::endl;
     t1.join(); t2.join();
 }
